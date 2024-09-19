8d077dbb93cd223f5d0a14c367e3bc45b7e39850
sook <jisuk0415@naver.com>
2024-09-19 16:15:54 +0900

    0919

---
+++ python_basic/함수실습문제.py
@@ -0,0 +1,34 @@
+# 입력 받은 수 미만의 소수의 합 구하기
+# 소수 : 1과 자기 자신을 제외하고 나누어지지 않는 수
+# 입력 : 12  (2 + 3 + 5 + 7 + 11) = 28
+
+# 내가 풀다 만 거 >>>>>>> 아직 다 못함...
+# def prime_func(n):
+#     nums = [x for x in range(2, n)]
+#     print(nums)
+#     for e in nums:
+#         for j in range(2, e):
+#             print(f"eeeeeeeee{e}")
+#             print(f"e : {e}  j : {j}")
+#             if e % j == 0:
+#                 print(f"이놈 지운다 {e}")
+#                 nums.remove(e)
+#                 break
+#         print(nums)
+#
+# num = int(input("정수 입력 : "))
+# prime_func(num)
+
+# 강사님 답
+def prime_func(n):
+    is_prime = True
+    for i in range(2, n):  # 1과 자기자신을 제외
+        if n % i == 0: is_prime = False
+    if is_prime: return n
+    else: return 0
+
+num = int(input("정수 입력 : "))
+prime_sum = 0
+for i in range(2, num):
+    prime_sum += prime_func(i)
+print(prime_sum)
